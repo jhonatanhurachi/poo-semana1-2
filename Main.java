815863e8aa2c721d7196e8622ddbf0827ed86395
Jhonatan Tupayachi Hurtado <jhonatanhurachi@gmail.com>
2026-04-22 22:04:09 -0500

    feat: agrego class Main con prueba de 3 estudiantes y metodo estatico

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Estudiante e1 = new Estudiante("Ana García", 1001, 16.5);
+        Estudiante e2 = new Estudiante("Luis Torres", 1002, 14.0);
+        Estudiante e3 = new Estudiante("Rosa Díaz",  1003, 18.0);
+ 
+        e1.mostrarInfo();
+        e2.mostrarInfo();
+        e3.mostrarInfo();
+
+        System.out.println("Total estudiantes: " + Estudiante.getTotalEstudiantes());
+    }
+}
